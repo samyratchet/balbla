f94a3d7bc3066f0fffe5f9844d4cffae976e4fe0
samyratchet <samy.ratchet@gmail.com>
2020-04-17 23:05:37 +0200

    Initial commit

---
+++ src/main/java/com/degitise/samyratchet/blabla/Main.java
@@ -0,0 +1,17 @@
+package com.degitise.samyratchet.blabla;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Noot Noot");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
